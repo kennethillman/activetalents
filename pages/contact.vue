1a1ce4abf3b3755a1eba6e9fd2d62729f87332b5
Kenneth Illman <kenneth.illman@gmail.com>
2019-11-12 10:05:34 +0530

    Netlify forms

--- pages/contact.vue
+++ pages/contact.vue
@@ -19,7 +19,7 @@
         <div class="gc">
           <div class="g-5 g-push-1 text two">
 
-
+<!--
             <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
               <input type="hidden" name="contact" value="ask-question" />
               <p>
@@ -35,6 +35,30 @@
               <p>
                 <button class="btn" type="submit">Send</button>
               </p>
+            </form> -->
+
+            <form
+              name="contact"
+              action="/thanks"
+              method="post"
+              netlify
+              netlify-honeypot="bot-field"
+            >
+              // Hidden input to check for bots
+              <input type="hidden" name="form-name" value="contactus" />
+              <div>
+                <label for="name">Name:</label>
+                <input type="text" name="name" required/>
+              </div>
+              <div>
+                <input type="email" name="email" required/>
+                <label for="email">Email:</label>
+              </div>
+              <div>
+                <textarea name="message" required></textarea>
+                <label for="message">Message:</label>
+              </div>
+              <button type="submit" value="Send message">Send</button>
             </form>
 
 
---
+++ pages/thanks.vue
@@ -0,0 +1,68 @@
+<template>
+
+  <div class="p-onsite -bg-gradient-blue" ref="pOnsite"
+        :class="{}">
+
+        <div class="custom-overlay -bg-pink"></div>
+
+    <section class="s- ">
+
+      <div class="section-top">
+        <div class="gc">
+          <div class="g-12">
+            <h2 class="header-primary -in-view"> Thank you for your meassage!</h2>
+          </div>
+        </div>
+      </div>
+
+      <div class="section-body">
+        <div class="gc">
+          <div class="g-5 g-push-1 text two">
+            Lorum
+          </div>
+          <div class="g-5 text two">
+            Lorum
+          </div>
+          <div class="g-12 -text-center three"><br><br><nuxt-link to="/" class="btn ">Home</nuxt-link></div>
+        </div>
+      </div>
+
+
+    </section>
+
+  </div>
+
+</template>
+
+
+<script>
+
+import cHeaderPrimary from '~/components/cHeaderPrimary.vue'
+import mTeaserText from '~/components/mTeaserText.vue'
+
+export default {
+  components: {
+    cHeaderPrimary,
+    mTeaserText
+  },
+  transition: 'custom',
+  data() {
+    return {
+      //lorum: false
+    };
+  },
+  methods: {
+    lorum() {
+
+    }
+  },
+  mounted () {
+      //this.lorum();
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+</style>
